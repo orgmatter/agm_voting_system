1171f97d3dcfab2976131918e51583f36778ce9c
ogadimma osuji <ogadimma.osuji@mkobo.com.ng>
2020-10-29 03:28:36 +0100

    modified login view and composer.json

--- resources/views/shareholder/login.blade.php
+++ resources/views/shareholder/login.blade.php
@@ -5,27 +5,34 @@
 @section('loginContent')
     <div class="login-content-container">
         <div class="login-content-cover">
-            <div class="login-form-cover-flex">
-                <div class="login-form-cover-item">
-                    <div class="login-form-cover">
-                        <form class="login-form" id="" action="{{ route('shareholder.login') }}" method="post">
-                            @csrf
-                            <div class="login-input-cover-flex">
-                                <div class="login-input-cover-item">
-                                    <label for="email-input">Email:</label><br />
-                                    <input class="login-input" id="email-input" type="text" name="email" placeholder="Email" required>
-                                </div>
-                                <div class="login-input-cover-item">
-                                    <label for="email-input">Password:</label><br />
-                                    <input class="login-input" id="password-input" type="password" name="password" placeholder="Password" required>
-                                </div>
+            <div class="parent-login-form-cover-flex">
+                <div class="parent-login-form-cover-item">
+                    <div class="form-header">
+                        <p class="header-title">Shareholder Login</p>
+                    </div>
+                    <div class="login-form-cover-flex">
+                        <div class="login-form-cover-item">
+                            <div class="login-form-cover">
+                                <form class="login-form" id="" action="{{ route('shareholder.login') }}" method="post">
+                                    @csrf
+                                    <div class="login-input-cover-flex">
+                                        <div class="login-input-cover-item">
+                                            <label for="email-input">Email:</label><br />
+                                            <input class="login-input" id="email-input" type="text" name="email" placeholder="Email" required>
+                                        </div>
+                                        <div class="login-input-cover-item">
+                                            <label for="email-input">Password:</label><br />
+                                            <input class="login-input" id="password-input" type="password" name="password" placeholder="Password" required>
+                                        </div>
+                                    </div>
+                                    <div class="btn-cover-flex">
+                                        <div class="btn-cover-item">
+                                            <button class="input-btn btn btn-primary" id="" type="submit">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                            <div class="btn-cover-flex">
-                                <div class="btn-cover-item">
-                                    <button class="input-btn btn btn-primary" id="" type="submit">Submit</button>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
